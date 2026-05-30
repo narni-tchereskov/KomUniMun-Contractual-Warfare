bb9293550659870fcaa9d34245c24ad593952208
Narni Tchereskov <naristratenov@gmail.com>
2026-05-30 05:53:34 -0300

    refactor: :fire: Removed the extension, adjusted spawn velocity
    spawn velocity reduces to 5m/s, addon extension separated into its own repository

--- KomUniMunCWPlugin/Project/Settings.cs
+++ KomUniMunCWPlugin/Project/Settings.cs
@@ -6,9 +6,9 @@ namespace KomUniMunVesselRectifier
     {
         public static bool VerboseLogs = true;
         public static int GHardeningDuration = 90;
-        public static float SpawnSpeed = 10f;
-        public static float HelicopterSpawnSpeed = 10f;
-        public static float AircraftSpawnSpeed = 10f;
+        public static float SpawnSpeed = 5f;
+        public static float HelicopterSpawnSpeed = 5f;
+        public static float AircraftSpawnSpeed = 5f;
         public static float MinAGL = 7500f;
         public static float MaxAGL = 10000f;
         public static float SceneSettleDuration = 5f;
